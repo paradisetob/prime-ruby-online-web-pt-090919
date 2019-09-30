8b3842e827e2818433b6d32404bab6b048c892b7
tracey brimage <onelovenilynn@gmail.com>
2019-09-30 04:36:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,4 +2,14 @@ def prime?(number)
   range =(1...(number -1)).to_a
   check=true
   if number < 2
-    chec
+    check = false
+   else
+     range.each do |x|
+       if number % x == 0
+         check = false
+         break
+       end
+     end
+   end
+    puts check
+ end
